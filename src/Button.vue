83d0da96f5c564699ad9130c9c41d926c3105df7
McCallWang <1059511459@qq.com>
2020-08-19 23:09:35 +0800

    Button 组件接收自定义 icon，以及判断 iconPosition

--- src/Button.vue
+++ src/Button.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <button class="i-button">
-        <i-icon></i-icon>
+    <button class="i-button" :class="{[`icon-${iconPosition}`]:true}">
+      <i-icon v-if="icon" class="icon" :icon-name="icon"></i-icon>
+      <div class="content">
         <slot/>
+      </div>
     </button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Button.vue'
+    name: 'Button.vue',
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return value === 'left' || value === 'right';
+        }
+      }
+    }
   }
 </script>
 
@@ -22,6 +34,9 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     &:hover {
       border-color: var(--border-color-hover);
@@ -33,6 +48,30 @@
 
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      margin-right: .1em;
+      order: 1;
+    }
+
+    > .content {
+      line-height: 1em;
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        margin-right: 0;
+        margin-left: .1em;
+        order: 2;
+      }
+
+      > .content {
+        line-height: 1em;
+        order: 1;
+      }
+
     }
 
 
--- src/Icon.vue
+++ src/Icon.vue
@@ -1,16 +1,24 @@
 <template>
-    <svg class="icon" aria-hidden="true">
-      <use xlink:href="#icon-download"></use>
+    <svg class="icon" aria-hidden="true" v-if="iconName">
+      <use :xlink:href=" `#icon-${iconName}` "></use>
     </svg>
 </template>
 
 <script>
   export default {
-    name: 'Icon.vue'
+    name: 'Icon.vue',
+    props: ['iconName']
   }
 </script>
 
 <style lang="scss" scoped>
 
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: middle;
+    fill: currentColor;
+    overflow: hidden;
+  }
 
 </style>
