9fa48736d529f166d095882efc5f9823340be199
McCallWang <1059511459@qq.com>
2020-08-20 13:08:42 +0800

    第一个用例测试（单元测试）：Button的icon测试

--- src/app.js
+++ src/app.js
@@ -5,14 +5,38 @@ import ButtonGroup from './ButtonGroup'
 
 Vue.component('i-button', Button)
 Vue.component('i-icon', Icon)
-Vue.component('i-button-group',ButtonGroup)
+Vue.component('i-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
-  data:{
-    loading1:true,
-    loading2:false,
-    loading3:true,
-    loading4:false,
+  data: {
+    loading1: true,
+    loading2: false,
+    loading3: true,
+    loading4: false,
   }
 })
+
+
+// 单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+
+{
+  // 未挂载状态
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })   // vm : Vue 实例
+
+  // 若不提供挂载参数，将被渲染到文档之外的元素
+  vm.$mount('#test')
+  const useElement = vm.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
