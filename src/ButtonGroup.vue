3593fd87c5fdd8e18c2e55bfe57189c2092db836
McCallWang <1059511459@qq.com>
2020-08-20 10:16:24 +0800

    添加 margin-left -1px，修复hover后border的重叠bug

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -17,6 +17,9 @@
     vertical-align: middle;
     > .i-button{
       border-radius: 0;
+      &:not(:first-child){
+        margin-left: -1px;
+      }
       &:first-child{
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
@@ -24,6 +27,10 @@
       &:last-child{
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index: 1;
       }
     }
   }
