916e8b059c19d277c892332540fff57138518839
McCallWang <1059511459@qq.com>
2020-08-24 22:46:04 +0800

    使用 npm test 和 npm dev-test 自动测试

--- src/Button.vue
+++ src/Button.vue
@@ -1,17 +1,22 @@
 <template>
-    <button class="i-button" :class="{[`icon-${iconPosition}`]:true}"
-            @click="$emit('click')"
-    >
-      <i-icon v-if="!loading" class="icon" :icon-name="icon"></i-icon>
-      <i-icon class="loading" v-if="loading" icon-name="loading"></i-icon>
-      <div class="content">
-        <slot/>
-      </div>
-    </button>
+  <button class="i-button" :class="{[`icon-${iconPosition}`]:true}"
+          @click="xxx"
+  >
+    <i-icon v-if="!loading" class="icon" :icon-name="icon"></i-icon>
+    <i-icon class="loading" v-if="loading" icon-name="loading"></i-icon>
+    <div class="content">
+      <slot/>
+    </div>
+  </button>
 </template>
 
 <script lang="js">
+  import Icon from './Icon'
+
   export default {
+    components: {
+      'i-icon': Icon
+    },
     name: 'Button.vue',
     props: {
       icon: {},
@@ -26,11 +31,25 @@
           return value === 'left' || value === 'right'
         }
       }
+    },
+    methods: {
+      xxx(e) {
+        console.log(e)
+        this.$emit('click')
+        let inX = e.clientX - e.target.offsetLeft
+        let inY = e.clientY - e.target.offsetTop
+        let button = document.querySelector('.i-button')
+        if (e) {
+          document.styleSheets[1].addRule('.i-button::after', 'display:block')
+        }
+      },
+      yyy() {
+      }
     }
   }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
   @keyframes spin {
     0% {
       transform: rotate(0deg)
@@ -41,17 +60,34 @@
   }
 
   .i-button {
-    font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
-    border-radius: var(--border-radius); border: 1px solid var(--border-color);
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
     background: var(--button-bg);
-    display: inline-flex; justify-content: center; align-items: center;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
     vertical-align: middle;
 
-    &:hover { border-color: var(--border-color-hover); }
+    &:after {
+      content: 'xxx';
+      color: red;
+      display: none;
+    }
 
-    &:active { border-color: var(--border-active-bg); }
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
 
-    &:focus { outline: none; }
+    &:active {
+      border-color: var(--border-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
 
     > .icon {
       margin-right: .1em;
