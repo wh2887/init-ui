5af7456569f6809962db217287ecf0c3adee01d4
McCallWang <1059511459@qq.com>
2020-08-30 13:45:57 +0800

    重构 Row 和 Col （提取变量法）

--- src/Col.vue
+++ src/Col.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="col" :class="[ span && `col-${span}`,offset && `offset-${offset}`]"
-       :style="{paddingLeft:gutter/2 + 'px',paddingRight: gutter/2 + 'px'}"
-  >
-    <div style="border: 1px solid green; height: 50px;">
+  <div class="col" :class="colClass" :style="colStyle" >
       <slot></slot>
-    </div>
   </div>
 </template>
 
@@ -22,6 +18,20 @@
     data() {
       return {
         gutter: 0
+      }
+    },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px', paddingRight: this.gutter / 2 + 'px'
+        }
       }
     }
   }
