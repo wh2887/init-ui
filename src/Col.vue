a7e7afe95092bfd8ad4fe6aabf156ad04ce230cf
McCallWang <1059511459@qq.com>
2020-08-31 10:31:25 +0800

    稍微重构了一下

--- src/Col.vue
+++ src/Col.vue
@@ -35,16 +35,31 @@
         gutter: 0
       }
     },
+    methods: {
+      createClasses(obj, str = '') {
+        if (!obj) {
+          return []
+        }
+        let array = []
+        if (obj.span) {
+          array.push(`col-${str}${obj.span}`)
+        }
+        if (obj.offset) {
+          array.push(`offset-${str}${obj.offset}`)
+        }
+        return array
+      }
+    },
     computed: {
       colClass() {
         let {span, offset, ipad, narrowPc, pc, widePc} = this
+        let {createClasses} = this
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          ipad && `col-ipad-${ipad.span}`,
-          narrowPc && `col-narrow-pc-${narrowPc.span}`,
-          pc && `col-pc-${pc.span}`,
-          widePc && `col-wide-pc-${widePc.span}`,
+          ...createClasses({span, offset}),
+          ...createClasses(ipad, 'ipad-'),
+          ...createClasses(narrowPc, 'narrow-pc-'),
+          ...createClasses(pc, 'pc-'),
+          ...createClasses(widePc, 'wide-pc-'),
         ]
       },
       colStyle() {
