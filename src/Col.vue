ae90a21d43b0d0f7745c8fd056d5f2415284476e
McCallWang <1059511459@qq.com>
2020-08-31 09:59:57 +0800

    响应式基本完成（ 漏传或不传值时 向宽度小的靠近）

--- src/Col.vue
+++ src/Col.vue
@@ -71,7 +71,7 @@
       }
     }
 
-    @media (min-width: 577px) and (max-width: 768px) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -85,7 +85,7 @@
         }
       }
     }
-    @media (min-width: 769px) and (max-width: 992px) {
+    @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -99,7 +99,7 @@
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
