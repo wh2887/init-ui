a1c6a83a7036396d4d1f8db63a8b7ae12b44dfb8
McCallWang <1059511459@qq.com>
2020-08-31 16:29:32 +0800

    添加简单动画

--- src/Sider.vue
+++ src/Sider.vue
@@ -1,17 +1,39 @@
 <template>
-    <div class="sider-wrapper">
+  <transition name="slide">
+    <div class="sider-wrapper" v-if="visible">
       <slot></slot>
+      <button @click="visible = false">close</button>
     </div>
+  </transition>
 </template>
 
 <script>
   export default {
     name: 'InitSider',
+    data(){
+      return{
+        visible:true
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   .sider-wrapper {
+    position: relative;
+
+    > button {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
   }
 
+  .slide-enter-active, .slide-leave-active {
+    transition: all 3s;
+  }
+
+  .slide-enter, .slide-leave-to {
+    margin-left: -200px;
+  }
 </style>
