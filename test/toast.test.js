303e5ef8083df0011d0d4b6ffc12abc4796d39f7
McCallWang <1059511459@qq.com>
2020-09-01 14:26:19 +0800

    完成 Toast 的 接收 autoClose 的用例测试

--- test/toast.test.js
+++ test/toast.test.js
@@ -11,6 +11,23 @@ describe('Toast', () => {
   })
 
   describe('props', () => {
+    it('接收 autoClose', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      const Constructor = Vue.extend(Toast)
+      const vm = new Constructor({
+        propsData: {
+          autoClose: true,
+          autoCloseDelay: 1
+        }
+      }).$mount(div)
+      vm.$on('beforeClose', () => {
+        expect(document.body.contains(vm.$el)).to.eq(false)
+        done()
+        vm.$el.remove()
+        vm.$destroy()
+      })
+    })
   })
 
 
