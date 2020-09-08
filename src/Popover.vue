cfa86edab9985e5b36db1e60df8382ff50bb916c
McCallWang <1059511459@qq.com>
2020-09-08 10:58:44 +0800

    支持两种事件触发

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div class="content-wrapper" ref="contentWrapper" v-if="visible"
          :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
@@ -26,8 +26,48 @@
         validator(value) {
           return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
         }
+      },
+      trigger: {
+        type: String,
+        default: 'click',
+        validator(value) {
+          return ['click', 'hover'].indexOf(value) >= 0
+        }
+      },
+    },
+    mounted () {
+      if (this.trigger === 'click') {
+        this.$refs.popover.addEventListener('click', this.onClick)
+      } else {
+        this.$refs.popover.addEventListener('mouseenter', this.open)
+        this.$refs.popover.addEventListener('mouseleave', this.close)
       }
     },
+      destroyed () {
+        if (this.trigger === 'click') {
+          this.$refs.popover.removeEventListener('click', this.onClick)
+        } else {
+          this.$refs.popover.removeEventListener('mouseenter', this.open)
+          this.$refs.popover.removeEventListener('mouseleave', this.close)
+        }
+      },
+      computed: {
+        openEvent () {
+          if (this.trigger === 'click') {
+            return 'click'
+          } else {
+            return 'mouseenter'
+          }
+        },
+        closeEvent () {
+          if (this.trigger === 'click') {
+            return 'click'
+          } else {
+            return 'mouseleave'
+          }
+        }
+      },
+
     methods: {
       positionContent() {
         const {contentWrapper, triggerWrapper} = this.$refs
