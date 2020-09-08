4ee580222b839b89684a01353575c0a625f342df
McCallWang <1059511459@qq.com>
2020-09-08 10:04:05 +0800

    重构 Tabs.vue

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -28,19 +28,29 @@
     provide() {
       return {eventBus: this.eventBus}
     },
-    mounted() {
-      if (this.$children.length === 0) {
-        console && console.warn && console.warn('i-tabs 组件必须有子组件！')
-      }
-      this.$children && this.$children.forEach((vm) => {
-        if (vm.$options.name === 'InitTabsHead') {
-          vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === 'InitTabsItem' && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
+    methods: {
+      checkChildren() {
+        if (this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
         }
-      })
+      },
+      selectTab () {
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'InitTabsHead') {
+            vm.$childiren.forEach((childVm) => {
+              if (childVm.$options.name === 'InitTabsItem'
+                && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted() {
+      this.checkChildren();
+      this.selectTab();
     }
   }
 </script>
