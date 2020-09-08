4eb024bdecde873f784336a1dd50941202c4bbba
McCallWang <1059511459@qq.com>
2020-09-08 10:19:04 +0800

    添加 popover 默认样式

--- src/Popover.vue
+++ src/Popover.vue
@@ -4,7 +4,7 @@
     <div class="content-wrapper" ref="contentWrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper" >
+    <span ref="triggerWrapper" class="trigger-wrapper">
       <slot></slot>
     </span>
   </div>
@@ -57,6 +57,8 @@
 </script>
 
 <style lang="scss" scoped>
+  $border-color:#333;
+  $border-radius:4px;
   .popover {
     display: inline-block;
     vertical-align: top;
@@ -65,8 +67,34 @@
 
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
     transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+  }
+  .trigger-wrapper{
+    display: inline-block;
   }
 </style>
