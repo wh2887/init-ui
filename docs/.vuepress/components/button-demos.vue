3d5ad34a8bce2e70e89cc0bca07c626d824ea403
McCallWang <1059511459@qq.com>
2020-09-09 15:29:08 +0800

    vuepress 实现 样式 与 代码显示

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
     <i-button>默认按钮</i-button>
+    <i-button icon="settings">默认按钮</i-button>
+    <i-button :loading="true">默认按钮</i-button>
+    <i-button disabled>默认按钮</i-button>
+    <pre>
+    <code> {{content}} </code>
+  </pre>
   </div>
+
 </template>
 
 <script>
   import Button from '../../../src/Button'
+
   export default {
     components: {
-      'i-button': Button
+      'i-button': Button,
+    },
+    data() {
+      return {
+        content: `
+            <i-button>默认按钮</i-button>
+            <i-button icon="settings">默认按钮</i-button>
+            <i-button :loading="true">默认按钮</i-button>
+            <i-button disabled>默认按钮</i-button>
+        `,
+      }
     }
   }
 
